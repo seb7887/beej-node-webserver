860e38d1d8b44c1c55c5d4cd63a2c10b3a7ec224
Sebastian <segurasebastian7887@gmail.com>
2019-03-07 15:08:46 -0300

    Socket.io Chat App

--- socket-io/server.js
+++ socket-io/server.js
@@ -2,6 +2,17 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
+const {
+  generateMessage,
+  generateLocationMessage
+} = require("./utils/messages");
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
@@ -12,8 +23,56 @@ const publicPath = path.join(__dirname, "./public");
 
 app.use(express.static(publicPath));
 
-io.on("connection", () => {
+io.on("connection", socket => {
   console.log("New Websocket connection");
+
+  socket.on("join", (options, callback) => {
+    const { error, user } = addUser({ id: socket.id, ...options });
+
+    if (error) {
+      return callback(error);
+    }
+
+    socket.join(user.room);
+
+    socket.emit("message", "Welcome!");
+    socket.broadcast
+      .to(user.room)
+      .emit("message", generateMessage(`${user.username} has joined`));
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed!");
+    }
+
+    io.to(user.room).emit("message", generateMessage(message));
+    callback();
+  });
+
+  socket.on("sendLocation", ({ latitude, longitude }, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit(
+      "locationMessage",
+      generateLocationMessage(
+        `https://google.com/maps?q=${latitude},${longitude}`
+      )
+    );
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        generateMessage(`${user.username} has left`)
+      );
+    }
+  });
 });
 
 server.listen(PORT, () => {
