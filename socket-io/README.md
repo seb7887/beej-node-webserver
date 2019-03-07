3f7518ec94a4de171aa1ad84af1ffcedab7e048c
Sebastian <segurasebastian7887@gmail.com>
2019-03-07 09:31:41 -0300

    Socket.io setup

---
+++ socket-io/README.md
@@ -0,0 +1,9 @@
+# Websocket Chat App with Socket.io
+
+- Websockets allow for full-duplex communication
+
+- In HTTP only the client can start de communication. In Websockets, server can start it too.
+
+* Websocket is a separate protocol from HTTP
+
+* Websocket maintains a persistent connection between client and server
--- socket-io/server.js
+++ socket-io/server.js
@@ -1,12 +1,21 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 9000;
+const socketio = require("socket.io");
 
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const PORT = process.env.PORT || 9000;
 const publicPath = path.join(__dirname, "./public");
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+io.on("connection", () => {
+  console.log("New Websocket connection");
+});
+
+server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
